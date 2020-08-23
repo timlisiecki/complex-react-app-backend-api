03f3db8244647e635382fb9c062280e8658a9de3
Tim <tim.lisiecki@gmail.com>
2020-08-23 16:39:11 -0400

    updated token expiration time

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const Follow = require('../models/Follow');
 const jwt = require('jsonwebtoken');
 
 // how long a token lasts before expiring
-const tokenLasts = '365d';
+const tokenLasts = '90d';
 
 exports.apiGetPostsByUsername = async function (req, res) {
 	try {
